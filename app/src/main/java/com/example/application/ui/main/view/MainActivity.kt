c7bba905463b29a818a5cadffd79eaee89bb8f84
TranLuan2608 <73928878+TranLuan2608@users.noreply.github.com>
2021-11-11 17:23:44 +0700

    11/11

--- app/src/main/java/com/example/application/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/application/ui/main/view/MainActivity.kt
@@ -10,12 +10,12 @@ import com.example.application.R
 import com.example.application_library.TrackingActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.analytics.FirebaseAnalytics
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_sheet.*
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var bsBehavior: BottomSheetBehavior<ConstraintLayout>
 
     private val firebaseAnalytics = FirebaseAnalytics.getInstance(this)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,27 +23,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-//        val track = findViewById<Button>(R.id.btnTracking)
-//        track.setOnClickListener {
-//            startActivity(Intent(this, TrackingActivity::class.java))
-//            val bundle = Bundle().apply {
-//                putString(FirebaseAnalytics.Param.ITEM_ID, "123")
-//                putString(FirebaseAnalytics.Param.ITEM_NAME, "Test View")
-//                putString(FirebaseAnalytics.Param.CONTENT_TYPE, "View")
-//            }
-//            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
-//        }
-
-
-        val bottomSheet = findViewById<ConstraintLayout>(R.id.bottomSheet)
-        bsBehavior  = BottomSheetBehavior.from(bottomSheet)
-
-        btnShowBottomSheet.setOnClickListener {
-            bsBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        val track = findViewById<Button>(R.id.btnTracking)
+        track.setOnClickListener {
+            startActivity(Intent(this, TrackingActivity::class.java))
+            val bundle = Bundle().apply {
+                putString(FirebaseAnalytics.Param.ITEM_ID, "123")
+                putString(FirebaseAnalytics.Param.ITEM_NAME, "Test View")
+                putString(FirebaseAnalytics.Param.CONTENT_TYPE, "View")
+            }
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
         }
-        tvCloseBS.setOnClickListener {
-            bsBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
+        btnBottomSheetDemo.setOnClickListener {
+            startActivity(Intent(this, BottomSheetActivity::class.java))
+        }
+        btnBiometricDemo.setOnClickListener {
+            startActivity(Intent(this, BiometricLoginActivity::class.java))
         }
 
     }
